448d03990c7b99f714b3349cfffdaea7f0ce5c37
Matheus Santana <matheuscloud2005@gmail.com>
2026-01-08 18:28:54 -0300

    Cogido em Js para a criação do relogio

---
+++ Principal/index.js
@@ -0,0 +1,52 @@
+const agora = new Date();
+
+//Código para resolver o fuso horário
+const opcoes = {
+    timeZone: "America/Sao_Paulo",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2=digit"
+}
+
+const dataHoraFormatada = agora.toLocaleDateString("pt-BR", opcoes);
+
+ // Função para atualizar o relogio
+ function atualizarRelogio(){
+    const agora = new Date();
+      const dataHora = agora.toLocaleString("pt-BR", {
+        timeZone: "America/Sao_Paulo",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+ });
+    document.getElementById("relogio").innerText = dataHora ;
+}
+
+setInterval(atualizarRelogio, 1000);
+atualizarRelogio(); //executa na hora que a página carrega
+
+
+//Código para montar a data
+const dia = doisDigitos(agora.getDade());
+const mes = doisDigitos(agora.getMonth() + 1);
+const ano = agora.getFullYear();
+
+
+function doisDigitos(numero) {
+    return numero < 10 ? "0" + numero : numero;
+}
+
+const dataFormatada = `${dia}/${mes}/${ano}`;
+
+//Código para montar a hora
+const horas = doisDigitos(agora.getHours());
+const minutos = doisDigitos(agora.getMinutes());
+const segundos = doisDigitos(agora.getSeconds());
+
+const horaFormatada = `${horas}:${minutos}:${segundos}`;
